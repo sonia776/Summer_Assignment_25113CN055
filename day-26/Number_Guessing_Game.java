0504fe3fb34d3461ad62a1f8216e0d79bd611983
Sonia <soniakumari00881@gmail.com>
2026-06-25 18:00:19 +0530

    update Number_Guessing_Game

--- day-26/Number_Guessing_Game.java
+++ day-26/Number_Guessing_Game.java
@@ -1,13 +1,11 @@
-import java.util.Random;
 import java.util.Scanner;
 
 public class Number_Guessing_Game {
     public static void main(String[] args) {
 
-        Random random = new Random();
         Scanner sc = new Scanner(System.in);
 
-        int secretNumber = random.nextInt(100) + 1;
+        int secretNumber = (int)Math.floor(Math.random()*100 +1 );
 
         int guess;
 
