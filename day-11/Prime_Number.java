7e118b69e825c3ae184e8a2eb8fcffec61afbdac
Sonia <soniakumari00881@gmail.com>
2026-08-03 22:02:20 +0530

    Fix formatting issues in Prime_Number.java

--- day-11/Prime_Number.java
+++ day-11/Prime_Number.java
@@ -7,7 +7,7 @@ public class Prime_Number {
          isPrime = false;
       }
 
-      for (int i = 2; i <= num / 2; i++) {
+      for (int i = 2; i <= num / 2; i++ ) {
          if (num % i == 0) {
             isPrime = false;
          }
